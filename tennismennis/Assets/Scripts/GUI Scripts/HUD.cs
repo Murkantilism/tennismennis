fb7dd2839dac22a634fd2537402e8b9de241969d
slynch0t <slynchot@Gmail.com>
2014-10-09 21:33:05 -0400

    tweaking player cards and working score back into main HUD

--- tennismennis/Assets/Scripts/GUI Scripts/HUD.cs
+++ tennismennis/Assets/Scripts/GUI Scripts/HUD.cs
@@ -9,9 +9,9 @@ public class HUD : MonoBehaviour {
 	string p1Character;
 	string p2Character;
 
-	Vector2 p1Pos = new Vector2(Screen.width/16,Screen.height/16);
-	Vector2 p2Pos = new Vector2(Screen.width*17/32, Screen.height/16);
-	Vector2 size = new Vector2(Screen.width*13/32,Screen.height*3/16);
+	Vector2 p1Pos = new Vector2(Screen.width/32,Screen.height*2/32);
+	Vector2 p2Pos = new Vector2(Screen.width*19/32, Screen.height*2/32);
+	Vector2 size = new Vector2(Screen.width*10/32,Screen.height*5/32);
 
 	float p1Mennis = 0;
 	float p2Mennis = 0;
@@ -40,28 +40,24 @@ public class HUD : MonoBehaviour {
 
 		//// Player 1 HUD
 		GUI.BeginGroup (new Rect (p1Pos.x, p1Pos.y, size.x, size.y));
-		GUI.DrawTexture(new Rect(0,0,Screen.width*3/32,Screen.height*3/16), swolesaurusPic);
-		GUI.Label (new Rect(Screen.width*3/32,Screen.height*1/32,Screen.width/4,Screen.height/16), swolesaurusTitle);
-		GUI.Box(new Rect(Screen.width*21/64,0,Screen.width*1/16,Screen.height*3/16), "100", scoreStyle);
+		GUI.DrawTexture(new Rect(0,0,Screen.width*3/32,Screen.height*5/32), swolesaurusPic);
+		GUI.Label (new Rect(Screen.width*3/32,Screen.height*3/64,Screen.width*7/32,Screen.height*2/32), swolesaurusTitle);
 	
 		// mennis meter:
-	//	GUI.DrawTexture (new Rect (Screen.width*3/32,Screen.height*3/32, Screen.width*7/32, size.y/3),progressBarEmpty);
-		GUI.BeginGroup (new Rect (0, 0, p1Mennis, size.y));
-		GUI.DrawTexture (new Rect (Screen.width*3/32,Screen.height*3/32, Screen.width*7/32, size.y/3),progressBarFull);
+		GUI.BeginGroup (new Rect (0,0,p1Mennis, size.y));
+		GUI.DrawTexture (new Rect (Screen.width*3/32,Screen.height*5/64, Screen.width*7/32, Screen.height*1/32),progressBarFull);
 		GUI.EndGroup ();
 		
 		GUI.EndGroup ();
 
 		//// Player 2 HUD
 		GUI.BeginGroup (new Rect (p2Pos.x, p2Pos.y, size.x, size.y));
-		GUI.DrawTexture(new Rect(0,0,Screen.width*3/32,Screen.height*3/16), swolesaurusPic);
-		GUI.Label (new Rect(Screen.width*3/32,Screen.height*1/32,Screen.width/4,Screen.height/16), swolesaurusTitle);
-		GUI.Box(new Rect(Screen.width*21/64,0,Screen.width*1/16,Screen.height*3/16), "100", scoreStyle);
-		
+		GUI.DrawTexture(new Rect(0,0,Screen.width*3/32,Screen.height*5/32), swolesaurusPic);
+		GUI.Label (new Rect(Screen.width*3/32,Screen.height*3/64,Screen.width*7/32,Screen.height*2/32), swolesaurusTitle);
+
 		// mennis meter:
-	//	GUI.DrawTexture (new Rect (Screen.width*3/32,Screen.height*3/32, Screen.width*7/32, size.y/3),progressBarEmpty);
-		GUI.BeginGroup (new Rect (0, 0, p2Mennis, size.y));
-		GUI.DrawTexture (new Rect (Screen.width*3/32,Screen.height*3/32, Screen.width*7/32, size.y/3),progressBarFull);
+		GUI.BeginGroup (new Rect (0,0,p2Mennis, size.y));
+		GUI.DrawTexture (new Rect (Screen.width*3/32,Screen.height*5/64, Screen.width*7/32, Screen.height*1/32),progressBarFull);
 		GUI.EndGroup ();
 
 		GUI.EndGroup ();
