a6dec429d3ab828ea4ecb9ffe43152d341d5feda
slynch0t <slynchot@Gmail.com>
2014-10-13 21:26:02 -0400

    refactoring menus to a grid system and adding art assets to character/stage select

--- tennismennis/Assets/Scripts/GUI Scripts/MainMenuOverlay.cs
+++ tennismennis/Assets/Scripts/GUI Scripts/MainMenuOverlay.cs
@@ -9,23 +9,26 @@ public class MainMenuOverlay : MonoBehaviour {
 		GUI.skin = customSkin;
 
 		// Background box
-		GUI.Box(new Rect(Screen.width/4,Screen.height/8,Screen.width/2,Screen.height*3/4), "Tennis Mennis");
+		GUI.BeginGroup (new Rect (Screen.width*8/32, Screen.height*6/32, Screen.width/2, Screen.height*20/32));
+		GUI.Box(new Rect(0,0,Screen.width/2,Screen.height*20/32),"");
+		GUI.Label (new Rect(Screen.width*3/32, Screen.height*3/32, Screen.width*10/32, Screen.height*4/32), "Tennis Mennis");
 
 		// Load the default scene
-		if(GUI.Button(new Rect(Screen.width*3/8,Screen.height*3/8, Screen.width/4,Screen.height/8), "Quick Match")) {
+		if(GUI.Button(new Rect(Screen.width*5/32,Screen.height*7/32, Screen.width*6/32,Screen.height*3/32), "Quick Match")) {
 			DontDestroyOnLoad(GameObject.Find("SaveSelections"));
 			Application.LoadLevel("Court1");
 		}
 
 		// Character Select -> Stage Select -> Begin match
-		if(GUI.Button(new Rect(Screen.width*3/8,Screen.height*1/2 + 10,Screen.width/4,Screen.height/8), "Custom Match")) {
+		if(GUI.Button(new Rect(Screen.width*5/32,Screen.height*11/32,Screen.width*6/32,Screen.height*3/32), "Custom Match")) {
 			DontDestroyOnLoad(GameObject.Find("SaveSelections"));
 			Application.LoadLevel("CharacterSelect");
 		}
 
 		// Quit button
-		if(GUI.Button(new Rect(Screen.width*3/8,Screen.height*5/8 + 20,Screen.width/4,Screen.height/8), "Quit")) {
+		if(GUI.Button(new Rect(Screen.width*5/32,Screen.height*15/32,Screen.width*6/32,Screen.height*3/32), "Quit")) {
 			Application.Quit();
 		}
+		GUI.EndGroup ();
 	}
 }
