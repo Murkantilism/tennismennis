f912101de5db72d8319a2180a5d033f4b2bf42c0
slynch0t <slynchot@Gmail.com>
2014-10-14 00:13:02 -0400

    resizing labels on character select screen

--- tennismennis/Assets/Scripts/GUI Scripts/CharacterSelectOverlay.cs
+++ tennismennis/Assets/Scripts/GUI Scripts/CharacterSelectOverlay.cs
@@ -8,8 +8,6 @@ public class CharacterSelectOverlay : MonoBehaviour {
 	public Texture2D SwoleLabel;
 	public Texture2D ShivaLabel;
 	public Texture2D FishLabel;
-	Rect backLayer;
-	Rect mainLayer;
 	
 	SaveSelections saveSelection;
 
@@ -23,27 +21,36 @@ public class CharacterSelectOverlay : MonoBehaviour {
 		GUI.Box (new Rect(0,0, Screen.width/2, Screen.height*20/32), "");
 		GUI.Label (new Rect(Screen.width*1/32,Screen.height*1/64,Screen.width*14/32,Screen.height*4/32), "P1 Character");
 
+
 		// Character Select buttons
-		if(GUI.Button(new Rect(0,Screen.height*3/32,Screen.width*8/32,Screen.height*17/64), DennisLabel)) {
+		if(GUI.Button(new Rect(0,Screen.height*3/32,Screen.width*8/32,Screen.height*17/64), "")) {
 			saveSelection.WriteCharacterSelection("Dennis");
 			DontDestroyOnLoad(saveSelection.gameObject);
 			Application.LoadLevel("CharacterSelect_P2");
 		}
-		if(GUI.Button(new Rect(Screen.width*8/32,Screen.height*3/32,Screen.width*8/32,Screen.height*17/64), SwoleLabel)) {
+		GUI.Label(new Rect(Screen.width*1/32,Screen.height*6/32,Screen.width*6/32,Screen.height*13/64), DennisLabel);
+
+		if(GUI.Button(new Rect(Screen.width*8/32,Screen.height*3/32,Screen.width*8/32,Screen.height*17/64), "")) {
 			saveSelection.WriteCharacterSelection("S. Racks");
 			DontDestroyOnLoad(saveSelection.gameObject);
 			Application.LoadLevel("CharacterSelect_P2");
 		}
-		if(GUI.Button(new Rect(0,Screen.height*23/64,Screen.width*8/32,Screen.height*17/64), ShivaLabel)) {
+		GUI.Label(new Rect(Screen.width*9/32,Screen.height*6/32,Screen.width*6/32,Screen.height*13/64), SwoleLabel);
+
+		if(GUI.Button(new Rect(0,Screen.height*23/64,Screen.width*8/32,Screen.height*17/64), "")) {
 			saveSelection.WriteCharacterSelection("SH1-V4");
 			DontDestroyOnLoad(saveSelection.gameObject);
 			Application.LoadLevel("CharacterSelect_P2");
 		}
-		if(GUI.Button(new Rect(Screen.width*8/32,Screen.height*23/64,Screen.width*8/32,Screen.height*17/64), FishLabel)) {
+		GUI.Label(new Rect(Screen.width*1/32,Screen.height*29/64,Screen.width*6/32,Screen.height*13/64), ShivaLabel);
+
+		if(GUI.Button(new Rect(Screen.width*8/32,Screen.height*23/64,Screen.width*8/32,Screen.height*17/64), "")) {
 			saveSelection.WriteCharacterSelection("Colonel Topspin");
 			DontDestroyOnLoad(saveSelection.gameObject);
 			Application.LoadLevel("CharacterSelect_P2");
 		}
+		GUI.Label(new Rect(Screen.width*9/32,Screen.height*29/64,Screen.width*6/32,Screen.height*13/64), FishLabel);
+
 		GUI.EndGroup();
 	}
 }
